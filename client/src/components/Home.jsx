c67311f606f562f79d41abcae53a07e748471673
anam2 <andynam1997@gmail.com>
2020-07-29 22:30:00 -0700

    changes to server.js

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -15,6 +15,7 @@ import Footer from "./Footer";
 import axios from "axios";
 
 function Home() {
+  const baseUrl = "";
   const [correctLogin, setCorrectLogin] = useState(true);
   const [redirect, setRedirect] = useState(false);
   const [users, setUsers] = useState([]);
@@ -27,7 +28,7 @@ function Home() {
   // Get's list of users from db
   useEffect(() => {
     setTimeout(() => {
-      axios.get("http://localhost:5000/user").then((response) => {
+      axios.get(baseUrl + "/user").then((response) => {
         setUsers([...response.data]);
       }, 1000);
     });
